56de2d3ab02a26933a5c3d28c164b699284255e6
Aditya Shukla <Shadowshukla76@gmail.com>
2025-07-10 23:38:29 +0530

    added delete comment

--- controller/CommentController.ts
+++ controller/CommentController.ts
@@ -6,7 +6,7 @@ import { prismaClient } from "../db/prisma.js";
 
 export const GetAllComments = CustomTryCatch(
   async (request: Request, response: Response, next: NextFunction) => {
-    const blogId = request.params;
+    const { blogId } = request.params;
 
     if (!blogId) {
       logger.error(`Blog ID Not Found`);
@@ -37,7 +37,7 @@ export const GetAllComments = CustomTryCatch(
 export const CreateComment = CustomTryCatch(
   async (request: Request, response: Response, next: NextFunction) => {
     const user = request.user;
-    const blogId = request.params;
+    const { blogId } = request.params;
 
     if (!blogId) {
       logger.error(`Blog ID Not Found`);
@@ -90,6 +90,67 @@ export const CreateComment = CustomTryCatch(
       message: "New Comment Has Been Created",
       success: true,
       createdComment,
+    });
+  }
+);
+
+export const DeleteComment = CustomTryCatch(
+  async (request: Request, response: Response, next: NextFunction) => {
+    const user = request.user;
+    const { commentId } = request.params;
+
+    if (!commentId) {
+      logger.error(`Comment ID Not Found`);
+      return next(new AppError(`Blog ID Not Found`, 401));
+    }
+    const isCommentExist = await prismaClient.comment.findFirst({
+      where: {
+        id: Number(commentId),
+      },
+    });
+    if (!isCommentExist) {
+      logger.error("Failed to find Comment");
+      return next(new AppError("Failed to find Comment", 404));
+    }
+
+    if (!user) {
+      logger.error(`Failed to get the authenticated user ${user}`);
+      console.log(`Failed to get the authenticated user ${user}`);
+      return next(
+        new AppError(`Failed to get the authenticated user ${user}`, 404)
+      );
+    }
+    const { sub } = user;
+    if (!sub) {
+      logger.error(`Failed to get the authenticated user ${sub}`);
+      console.log(`Failed to get the authenticated user ${sub}`);
+      return next(
+        new AppError(`Failed to get the authenticated user ${sub}`, 404)
+      );
+    }
+    const userFound = await prismaClient.user.findUnique({
+      where: { id: sub },
+    });
+    if (!userFound) {
+      logger.error(`User With Id Do Not Exist: ${sub}`);
+      console.log(`User With Id Do Not Exist: ${sub}`);
+      return next(new AppError(`User With Id Do Not Exist: ${sub}`, 404));
+    }
+
+    if (userFound.id !== isCommentExist.authorId) {
+      logger.error(`You are not allowed to delete comment`);
+      console.log(`You are not allowed to delete comment`);
+      return next(new AppError(`You are not allowed to delete comment`, 404));
+    }
+
+  await prismaClient.comment.delete({
+      where: {
+        id:Number(commentId)
+      },
+    });
+    return response.status(200).json({
+      message: "Comment Has Been Deleted",
+      success: true,
     });
   }
 );
--- router/commentRouter.ts
+++ router/commentRouter.ts
@@ -3,6 +3,7 @@ import express from "express";
 import { authMiddleware } from "../middlewares/AuthCheck.js";
 import {
   CreateComment,
+  DeleteComment,
   GetAllComments,
 } from "../controller/CommentController.js";
 
@@ -10,3 +11,4 @@ export const CommentRouter = express.Router();
 
 CommentRouter.get("/:blogId", GetAllComments);
 CommentRouter.post("/:blogId", authMiddleware, CreateComment);
+CommentRouter.delete("/comment/:commentId",authMiddleware,DeleteComment)
