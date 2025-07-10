c0cf4f81f68942dc0cdc60ba5dd9c5b71d58ca50
Aditya Shukla <Shadowshukla76@gmail.com>
2025-07-10 21:45:56 +0530

    added comment router

---
+++ router/commentRouter.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+import { authMiddleware } from "../middlewares/AuthCheck.js";
+
+
+export const CommentRouter = express.Router();
+
+CommentRouter.get("/", );
--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { logger } from "./utils/logger.js";
 import { UserRouter } from "./router/UserRouter.js";
 import { BlogRouter } from "./router/BlogRouter.js";
+import { CommentRouter } from "./router/commentRouter.js";
 
 const app = express();
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT;
 
 app.use("/api/users",UserRouter)
 app.use("/api/blogs",BlogRouter)
+app.use("/api/comments",CommentRouter)
 
 
 app.listen(PORT, () => {
